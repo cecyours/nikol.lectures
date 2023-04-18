35d44c1b4d3782d38dca922a369c6e6b728044ed
gkhell <gpkmyworld@gmail.com>
2023-04-18 19:09:29 +0530

    18/04/2023⠀⎦⎡⠀19:09⠀⎦⎢⠀Tuesday

---
+++ python_2107/CH02_P06_tuple.py
@@ -0,0 +1,9 @@
+# you can't change or modify the tuple data:
+# () used to indicate the tuple.
+
+data = ("Krisha","Shivu","Mohan","Riya","kailash")
+
+# data[2] = "Harsh"
+
+print(data)
+print(data[::2])
---
+++ python_2107/CH02_P07_dictionary.py
@@ -0,0 +1,15 @@
+import clear
+
+marks = {
+        "harsh":{"name":"harsh","mark":78,"subject":"english"},
+        "sumit":399,
+        "gk":402,
+        "mohan":488,
+        "shiv":200
+        }
+
+print(marks)
+print(marks["shiv"])
+
+for i in marks:
+    print(i,marks[i])

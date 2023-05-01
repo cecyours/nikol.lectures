6231d54614b7217aa4b5951484eee6bb8cbbc2ab
gkhell <gpkmyworld@gmail.com>
2023-05-01 20:50:21 +0530

    01/05/2023⠀⎦⎡⠀20:50⠀⎦⎢⠀Monday

--- DawnofJava/CH01_P02_HelloWorld.java
+++ DawnofJava/CH01_P02_HelloWorld.java
@@ -1,9 +1,23 @@
 // compile(Check -> excutable)-> RUN
-class HelloWorld{
+class HelloSneha{
 
-    public static main(String x[])
+    public static void main(String x[])
     {
-        System.out.println("Hello Sneha.");
+        System.out.println("Hello Sneha, we are here to explore.35..");
+  
     }
 
 }
+
+// Run program
+/**
+ * 1 : compile & run 
+ * javac FileName.java
+ * java ClassFileName
+ * */ 
+
+ /**
+ * 2 : Run 
+ * java FileName.java
+ * */ 
+
---
+++ DawnofJava/WhyPythonDontSupport.py
@@ -0,0 +1,21 @@
+
+# Hello World..
+
+x = """
+This is my comment..
+"""
+
+y = "Hii im gk!!"
+
+z = 2398783279823.23
+
+print(x,y,z)
+
+def demo():
+    """
+        i am gk!!
+    """
+    print("How are you...")
+
+x = demo.__doc__
+print(x)

4902835129a07b172e7df87a89673b6adece6303
gkhell <gpkmyworld@gmail.com>
2023-05-02 12:59:20 +0530

    02/05/2023⠀⎦⎡⠀12:59⠀⎦⎢⠀Tuesday

---
+++ web-app-django3568/python/CH02_P02_variables.py
@@ -0,0 +1,13 @@
+import clear
+# variable is used to store a value for a while.
+# syntax
+# var_name = value
+
+a = 10
+print(a)
+
+# define variable/ rules of identifies.;
+# 1 : doesn't start with digit
+# 2 : it contains only alphabets, underscore, digits(not at begins.)
+# 3 : no more special symbols except underscore.
+
---
+++ web-app-django3568/python/CH02_P03_numeric_data_type.py
@@ -0,0 +1,12 @@
+import clear
+
+#  int
+# b = int(input("Enter the number : "))
+
+# c = a**b 
+# print(a," ** ",b," = ",c)
+
+
+a = int(input("Enter the number : "))
+a*=-1
+print(a)

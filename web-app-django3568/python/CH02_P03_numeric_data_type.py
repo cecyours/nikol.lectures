3144d25fd2815b92d1074136739ea6be4b621485
gkhell <gpkmyworld@gmail.com>
2023-05-03 11:10:35 +0530

    03/05/2023⠀⎦⎡⠀11:10⠀⎦⎢⠀Wednesday

--- web-app-django3568/python/CH02_P03_numeric_data_type.py
+++ web-app-django3568/python/CH02_P03_numeric_data_type.py
@@ -8,6 +8,20 @@ import clear
 
 
 a = int(input("Enter the number : "))
-a*=-1
+if a<0:
+    a*=-1
 print(a)
 # Task : perform a string program
+
+
+f = float(input("Enter the number : "))
+print(f)
+
+# complex .
+
+x = 9+8j
+z = complex(input("Enter complex value : "))
+y = type(x)
+print(x,y)
+
+print(x," + ",z," = ",(x-z))

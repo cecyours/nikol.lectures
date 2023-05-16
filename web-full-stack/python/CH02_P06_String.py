572daf745d1c5e5df8c6e1764353667d67192750
gkhell <gpkmyworld@gmail.com>
2023-05-16 13:04:17 +0530

    16/05/2023⠀⎦⎡⠀13:04⠀⎦⎢⠀Tuesday

---
+++ web-full-stack/python/CH02_P06_String.py
@@ -0,0 +1,36 @@
+
+
+
+# s = 'H'
+# print(s,type(s))
+
+# s = " Hello ed"
+# sm = """
+#     this is my code
+# """
+# #dsgdsg
+
+
+# print(s,sm)
+s = '''
+sdfsd
+dsfdsf
+'''
+
+# print(s)
+
+s = "1234567890"
+
+# indexing
+print(s[-1])
+
+print(s[4])
+
+
+# slicing : multiple character
+# start:end:step
+print(s[2:5])
+
+print(s[2:])
+
+print(s[1:5:2])
---
+++ web-full-stack/python/CH02_P07_list.py
@@ -0,0 +1,19 @@
+
+
+data = ["apple","banana",38,"234",89+4j,9.2]
+# start:end:step
+print(data[3])
+print(data[-1])
+print(data[1:4])
+print(data[::2])
+print(data[-1:0:-1])
+
+print("-----------------------")
+for i in data:
+    print("Hello",i*2)
+
+
+# TASK
+# perform all operation/methods of string
+#  perform class/session practicals
+# display all movies list with awsome prefix. 

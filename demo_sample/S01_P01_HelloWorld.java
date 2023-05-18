1ef1247cbe52efa03970be5699e6511fb1337135
gkhell <gpkmyworld@gmail.com>
2023-05-18 11:59:13 +0530

    18/05/2023⠀⎦⎡⠀11:59⠀⎦⎢⠀Thursday

---
+++ demo_sample/S01_P01_HelloWorld.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.util.Scanner;
+
+/**
+ * S01_P01_HelloWorld
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World..");
+
+    }
+}
+
+class Student{
+
+    
+    
+    // constructors....
+    // default
+    // TODO : i am fine.
+    // FIXME : not working...
+}

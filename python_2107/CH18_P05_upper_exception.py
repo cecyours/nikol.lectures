0c7870c3b45c2bb66b3c5d0c3c7eb88b950d91bf
gkhell <gpkmyworld@gmail.com>
2023-05-29 19:03:05 +0530

    29/05/2023⠀⎦⎡⠀19:03⠀⎦⎢⠀Monday

---
+++ python_2107/CH18_P05_upper_exception.py
@@ -0,0 +1,18 @@
+import clear
+
+class MyException(Exception):
+    def __init__(self,msg):
+        self.msg = msg
+        pass
+    def __str__(self):
+        return self.msg
+
+username = input("Enter a nume : ")
+
+try:
+
+    if(username.isupper()):
+        raise MyException("due to uppercase")
+    print("welcome ",username)
+except MyException as my:
+    print("error ",my)

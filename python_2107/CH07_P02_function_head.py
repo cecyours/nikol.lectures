2d738f7564d171bafe674f6694a230e47db761e4
gkhell <gpkmyworld@gmail.com>
2023-06-06 18:51:11 +0530

    06/06/2023⠀⎦⎡⠀18:51⠀⎦⎢⠀Tuesday

---
+++ python_2107/CH07_P02_function_head.py
@@ -0,0 +1,10 @@
+
+def isPassed(marks): # heading
+    # body part
+    if marks>=33:
+        return True
+    return False
+
+mark = int(input("Enter marks : "))
+x = isPassed(mark)
+print("status : ",x)
---
+++ python_2107/CH07_P03_return_statement.py
@@ -0,0 +1,9 @@
+
+def sum(a,b):
+    return a+b
+
+x = 10
+y = 29
+z = sum(x,y)
+
+print(z)

4395c88e715c3c8d0b772b85379675e4b53df126
gkhell <gpkmyworld@gmail.com>
2023-06-07 17:30:24 +0530

    07/06/2023⠀⎦⎡⠀17:30⠀⎦⎢⠀Wednesday

---
+++ core_world13/cpp-programming/CH01_P01_helloWorld.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    cout<<"Hello World"<<endl;
+    cout<<"my name is khan "<<(char)'-';
+    
+    return 'A';
+
+}
---
+++ core_world13/cpp-programming/CH01_P02_ASCII.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int start,end,i;
+// happy me :)
+    cout<<"Enter start end number ";
+    cin>>start>>end;
+
+    for(i=start;i<=end;i++)
+    {
+        cout<<" "<<i<<" : "<<(char)i<<endl;
+    }
+
+}
+

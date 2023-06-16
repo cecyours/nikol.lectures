f675c413e13788e77098d11e0832bf69d8e68ee1
gkhell <gpkmyworld@gmail.com>
2023-06-16 11:54:01 +0530

    16/06/2023⠀⎦⎡⠀11:53⠀⎦⎢⠀Friday

---
+++ python_2107/CH07_P10_implement.py
@@ -0,0 +1,5 @@
+
+
+import CH07_P10_module 
+
+CH07_P10_module.display("gk")
---
+++ python_2107/CH07_P10_module.py
@@ -0,0 +1,8 @@
+
+def display(username):
+    print("welcome "+username)
+
+
+
+if "__main__"==__name__:
+    display("harsh")

67495da2edac19f8598c0f8c4f91595e858805d9
cecyoursnikol <nikol.cecyours@gmail.com>
2023-07-17 23:26:48 -0700

    17/07/2023⠀⎦⎡⠀23:26⠀⎦⎢⠀Monday

---
+++ WEB_Rudra/calc.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+    int a , b;
+    char c;
+    printf("Enter the operation u want(+,-,*,/,%)");
+    scanf("%c",&c);
+    printf("Now Enter the value which u to perform opreation:");
+    printf("Enter the second value:");
+    if(c=='+')
+    {
+        a+b;
+        printf("%d",a+b);
+    }
+}

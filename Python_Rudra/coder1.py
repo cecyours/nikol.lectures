9f35d0559c800b5b680d70a77abbdd41e20a5bd6
cecyoursnikol <nikol.cecyours@gmail.com>
2023-07-25 03:09:47 -0700

    25/07/2023⠀⎦⎡⠀03:09⠀⎦⎢⠀Tuesday

---
+++ Python_Rudra/coder1.py
@@ -0,0 +1,14 @@
+a =  "rudra"
+i=0
+for n in a:
+                    
+    print(n)
+    if n == 'r':
+        break
+    
+    
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+#   if x == "banana":
+#     break
---
+++ Python_Rudra/func.py
@@ -0,0 +1,25 @@
+# def rudra():
+#         print("hey there is me :::")
+# def a(b):
+#         print(b+"rudra")
+# def s(*rudra):
+#         print("hey MR "+rudra[0])
+# def r(a , b  ,c ):
+#         print("there is "+ c)
+# r(a = "man", b = "womean", c = "trans")        
+# # s("Rudra","sam","akhil")
+
+# # rudra()
+# # rudra()
+# # a("rudra")
+# # a("SO WONDER")
+
+
+x = input(print('enter the number'))
+def my_function(x):
+  return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+

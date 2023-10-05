badf593c11a42089fc704c4e9ce49f004f91f556
gkHell <gpkmyworld@gmail.com>
2023-10-05 13:09:01 +0530

    05/10/2023⠀⎦⎡⠀13:08⠀⎦⎢⠀Thursday

--- DawnofJava/CH19_P11_Register.java
+++ DawnofJava/CH19_P11_Register.java
@@ -12,13 +12,19 @@ public class CH19_P11_Register  extends JFrame {
     JPanel formPanel;
     JPanel menuPanel;
 
-    JLabel lblUserName,lblUserEmail,lblUserPass,lblUserAddress,lblUserProgramming;
+    JLabel lblUserName,lblUserEmail,lblUserPass,lblUserAddress,lblUserProgramming,lblUserRole,lblUserQualification,lblUserCourse;
     JTextField txtUserName,txtUserEmail;
     JPasswordField pwdUserPass;
     JTextArea txtUserAddress;
 
     JCheckBox cbPython,cbJava,cbCpp,cbSharp;
 
+    JRadioButton rbProgrammer,rbCoder, rbLearner;
+    ButtonGroup grpRole;
+
+    JComboBox cmbUserQualification;
+
+    JList lstUserCourse;
 
     public CH19_P11_Register()
     {
@@ -43,7 +49,7 @@ public class CH19_P11_Register  extends JFrame {
         mainPanel.add(middlePanel,BorderLayout.CENTER);
 
         formPanel = new JPanel();
-        formPanel.setLayout(new GridLayout(6,2));
+        formPanel.setLayout(new GridLayout(8,2));
 
         lblUserName = new JLabel("Enter User Name : ");
         lblUserName.setHorizontalAlignment(SwingConstants.RIGHT);
@@ -78,7 +84,8 @@ public class CH19_P11_Register  extends JFrame {
         lblUserProgramming = new JLabel("Programming");
         lblUserProgramming.setHorizontalAlignment(SwingConstants.RIGHT);
 
-        cbPython = new JCheckBox("Python");        
+        cbPython = new JCheckBox("Python");
+        cbPython.setSelected(true);        
         cbJava = new JCheckBox("Java");
         cbCpp = new JCheckBox("C++");
         cbSharp = new JCheckBox("C#");
@@ -93,11 +100,51 @@ public class CH19_P11_Register  extends JFrame {
         programming.add(cbJava);
         programming.add(cbCpp);
         programming.add(cbSharp);
-        
-
-        
         formPanel.add(programming);
 
+        // TODO : 4th Oct 2023
+        lblUserRole = new JLabel("Select Role : ");
+        lblUserRole.setHorizontalAlignment(SwingConstants.RIGHT);
+        formPanel.add(lblUserRole);
+        
+        rbProgrammer = new JRadioButton("Programming");
+        rbProgrammer.setSelected(true);
+        rbCoder = new JRadioButton("Coder");
+        rbLearner = new JRadioButton("Learner");
+        
+        grpRole = new ButtonGroup();
+        grpRole.add(rbProgrammer);
+        grpRole.add(rbCoder);
+        grpRole.add(rbLearner);
+
+        JPanel role = new JPanel();
+        role.setLayout(new FlowLayout());
+
+        role.add(rbProgrammer);
+        role.add(rbCoder);
+        role.add(rbLearner);
+
+
+        formPanel.add(role);
+
+        lblUserQualification = new JLabel("Select your Qualification : ");
+        lblUserQualification.setHorizontalAlignment(SwingConstants.RIGHT);
+        formPanel.add(lblUserQualification);
+
+        String data[] = {"B.Tech","MBA","B.COM"};
+        cmbUserQualification = new JComboBox(data);
+        formPanel.add(cmbUserQualification);
+
+
+        lblUserCourse = new JLabel("Enter course : ");
+        lblUserCourse.setHorizontalAlignment(SwingConstants.RIGHT);
+        formPanel.add(lblUserCourse);
+
+        String list[] = {"Noob Programmer","Extra Bug","Lazy Programmer"};
+        lstUserCourse = new JList<>();
+        lstUserCourse.setVisibleRowCount(10);
+        
+        formPanel.add(lstUserCourse);
 
         middlePanel.add(formPanel);
 

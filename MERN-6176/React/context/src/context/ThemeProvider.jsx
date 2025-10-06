7d54eea106dc692bc31172b1d8bc52b7bc5da58b
vish0201 <rabariv832@gmail.com>
2025-10-06 15:47:47 +0530

    context code

---
+++ MERN-6176/React/context/src/context/ThemeProvider.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+// Export both the Provider and Context for flexibility
+export { ThemeContext };
+export default ThemeProvider;

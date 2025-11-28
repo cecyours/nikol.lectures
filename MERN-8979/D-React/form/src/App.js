f9a6560df9d269b3e4b5a2b11b6d26dd7ed66869
vish0201 <rabariv832@gmail.com>
2025-11-28 10:49:52 +0530

    uncontolled

--- MERN-8979/D-React/form/src/App.js
+++ MERN-8979/D-React/form/src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Controlled from './components/Controlled';
+import UnControlled from './components/UnControlled';
 
 function App() {
   return (
     <div className=' '>
       <Controlled />
+
+
+      <UnControlled />
     </div>
   );
 }
--- MERN-8979/D-React/form/src/components/UnControlled.jsx
+++ MERN-8979/D-React/form/src/components/UnControlled.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useRef } from "react";
 
 const UnControlled = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const inputRef = useRef(null);
 
-export default UnControlled
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Input value:", inputRef.current.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Type something" ref={inputRef} />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default UnControlled;

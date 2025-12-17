f18e101dbdf937f93b78f8a740be195e32db159f
vish0201 <rabariv832@gmail.com>
2025-12-17 11:20:34 +0530

    router

--- MERN-8979/D-React/router/src/App.js
+++ MERN-8979/D-React/router/src/App.js
@@ -1,44 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
-
-
-  const [todos, setTodos] = useState([])
-
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(response => response.json())
-      .then(json => setTodos(json))
-  }, [])
-
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-
-          {todos.map((t, index) => (
-            <div>
-              {t.title}
-              < hr />
-            </div>
-          ))}
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about-us" element={<About />} />
+          <Route path="/contact-us" element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
 }

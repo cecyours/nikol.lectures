3118afee5088926152b5b980577f43145045d32a
vish0201 <rabariv832@gmail.com>
2026-01-02 16:25:55 +0530

    loops

---
+++ python_dj/python/for_loops.py
@@ -0,0 +1,24 @@
+
+
+names = ["nishit" , "vishal" , "kuldeep"]
+
+
+
+for name in names:
+    print(f"{name} ")
+
+
+
+# to range
+for i in range(10):
+    print(i)
+
+
+# start and stop
+for i in range(0 , 10):
+    print(i)
+
+
+# steps
+for i in range(0  , 100 , 10):
+    print(i)
---
+++ python_dj/python/while_loop.py
@@ -0,0 +1,11 @@
+
+while True:
+      user_input = input("Enter a number (or quit for exit) : ")
+
+      if user_input =="quit":
+         break
+      print(f"You entered: {user_input}")
+
+
+  
+
